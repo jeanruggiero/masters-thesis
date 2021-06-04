556e6d108ef325f47930cdb6923f4e57c8b80eea
Jean Ruggiero <jeanruggiero@gmail.com>
2021-06-03 21:49:32 -0700

    Fixed bug in metrics calculation

--- predict_real_data.py
+++ predict_real_data.py
@@ -17,7 +17,7 @@ logging.basicConfig(level=logging.INFO, handlers=[
 import numpy as np
 
 from real_data_wrangling.pipelines import preprocess_real_data
-from modeling.metrics import f1_score
+from modeling.metrics import f1_score_post_epoch, precision_post_epoch, recall_post_epoch
 from modeling.modeling import expand_dim
 
 alpha = 0.05
@@ -62,5 +62,6 @@ print(f"y.shape = {y_test.shape}")
 y_pred_proba = model.predict(X_test)
 y_pred = np.argmax(y_pred_proba, axis=1)
 
-score = f1_score(y_test, y_pred)
-print(f"f1-score = {score}")
+print(f"f1-score = {f1_score_post_epoch(y_test, y_pred)}")
+print(f"precision = {precision_post_epoch(y_test, y_pred)}")
+print(f"recall = {recall_post_epoch(y_test, y_pred)}")
