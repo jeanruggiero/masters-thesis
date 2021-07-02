02f2f5ae658f3eb250811ef749f9c7d5b459bdbd
Jean Ruggiero <jeanruggiero@gmail.com>
2021-07-02 09:23:08 -0700

    Fixed bug in noise application

--- modeling/modeling.py
+++ modeling/modeling.py
@@ -56,7 +56,7 @@ def train_model(model, data_generator, output_time_range, sample_rate, callbacks
     logging.info(f'y_val.shape = {y_val.shape}')
 
     if noiser:
-        X_val = np.apply_along_axis(Noiser.noise, 0, X_val)
+        X_val = np.apply_along_axis(noiser.noise, 0, X_val)
 
     logging.info(f'X_val.shape = {X_val.shape}')
     logging.info(f'y_val.shape = {y_val.shape}')
@@ -87,7 +87,7 @@ def train_model(model, data_generator, output_time_range, sample_rate, callbacks
         X_train, y_train = preprocess(data_generator.generate_batch(i), output_time_range, sample_rate, resample=resample)
 
         if noiser:
-            X_train = np.apply_along_axis(Noiser.noise, 0, X_train)
+            X_train = np.apply_along_axis(noiser.noise, 0, X_train)
 
         logging.info(f'X_train.shape = {X_train.shape}')
         logging.info(f'y_train.shape = {y_train.shape}')
