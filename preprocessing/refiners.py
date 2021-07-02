ac6d6a099c8d4efcaaf1a544bca9ec26ac93dbff
Jean Ruggiero <jeanruggiero@gmail.com>
2021-07-02 10:12:13 -0700

    Removed print statements

--- preprocessing/refiners.py
+++ preprocessing/refiners.py
@@ -37,22 +37,6 @@ class Noiser:
         return np.apply_along_axis(self.extract_noise, 0, self.normalize(scan))
 
     def noise(self, simulated_scan):
-        print(f"Simulated scan shape: {simulated_scan.shape}")
-        print(f"self.real_scans[np.random.choice(self.real_scans.shape[0]).shape = "
-              f"{self.real_scans[np.random.choice(self.real_scans.shape[0])].shape}")
-
-        noise = self.extract_noise_bscan(
-            self.real_scans[np.random.choice(self.real_scans.shape[0])]
-        )
-
-        print(f"noise.shape = {noise.shape}")
-
-        noised = self.normalize(simulated_scan) + self.extract_noise_bscan(
-            self.real_scans[np.random.choice(self.real_scans.shape[0])]
-        )
-
-        print(f"noised.shape = {noised.shape}")
-
         return self.normalize(simulated_scan) + self.extract_noise_bscan(
             self.real_scans[np.random.choice(self.real_scans.shape[0])]
         )
