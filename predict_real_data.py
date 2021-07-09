fbc03dfed1cd9cdf17479aa40d7e7eef8bd593c6
Jean Ruggiero <jeanruggiero@gmail.com>
2021-07-09 16:40:35 -0700

    Updated code to load all models from all experiments and generate predictions using each.

--- predict_real_data.py
+++ predict_real_data.py
@@ -9,20 +9,32 @@ from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.metrics import Precision, Recall
 import boto3
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-# Configure logging
-logging.basicConfig(level=logging.INFO, handlers=[
-    logging.FileHandler("predictions.log"),
-    logging.StreamHandler()
-])
-
 import numpy as np
 
 from real_data_wrangling.pipelines import preprocess_real_data
 from modeling.metrics import f1_score_post_epoch, precision_post_epoch, recall_post_epoch
 from modeling.modeling import expand_dim
 
-def predict_real(model, experiment_name):
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# Configure logging
+fh = logging.FileHandler("training.log")
+fh.setLevel(logging.INFO)
+
+fhv = logging.FileHandler("training_verbose.log")
+fhv.setLevel(logging.DEBUG)
+
+sh = logging.StreamHandler()
+sh.setLevel(logging.INFO)
+
+logging.basicConfig(level=logging.INFO, handlers=[fh, fhv, sh])
+
+
+def predict_real(model, experiment_name, X_test, y_test):
+
+    logging.info('.........................................................................................')
+    logging.info("Generating predictions for experiment: " + experiment_name)
+
 
     bucket_name = "jean-masters-thesis"
 
@@ -30,13 +42,25 @@ def predict_real(model, experiment_name):
     bucket = s3.Bucket(name=bucket_name)
 
     # Get all objects pertaining to specified model
-    objs = bucket.objects.filter(Prefix='models/' + experiment_name)
+    objs = bucket.objects.filter(Prefix='models/' + experiment_name + '.')
 
     for obj in objs:
         bucket.download_file(obj.key, 'models/' + obj.key)
 
     # Restore the weights
     model.load_weights(experiment_name)
+
+    y_pred_proba = model.predict(X_test)
+    y_pred = np.argmax(y_pred_proba, axis=1)
+
+    logging.debug("y_test, y_pred, y_pred_proba")
+    for yt, yp, ypp in zip(y_test, y_pred, y_pred_proba):
+        logging.debug(f"{yt:5.3f}  {yp:5.3f}  {ypp:5.3f}")
+
+    logging.info(f"f1-score = {f1_score_post_epoch(y_test, y_pred_proba):.2f}")
+    logging.info(f"precision = {precision_post_epoch(y_test, y_pred_proba):.2f}")
+    logging.info(f"recall = {recall_post_epoch(y_test, y_pred_proba):.2f}")
+
 
 def load_real_data():
 
@@ -97,95 +121,43 @@ def load_real_data():
     logging.info(f"Total positive: {n_positive}")
     logging.info(f"Total negative: {n_negative}")
 
+    return X_test, y_test
 
-    y_pred_proba = model.predict(X_test)
-    y_pred = np.argmax(y_pred_proba, axis=1)
+if __name__ == '__main__':
 
-    print(y_test)
-    print(y_pred)
+    X_test, y_test = load_real_data()
 
-    print(y_pred_proba)
+    alpha = 0.05
+    model = keras.models.Sequential([
+        #keras.layers.InputLayer(shape=[144, 480, 1]),
+        keras.layers.BatchNormalization(),
+        keras.layers.Conv2D(filters=10, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
+                            activation='relu', padding='same'),
+        keras.layers.MaxPool2D(pool_size=(3, 3), strides=(1, 1)),
+        keras.layers.Conv2D(filters=20, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
+                            activation='relu', padding='same'),
+        keras.layers.Conv2D(filters=20, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
+                            activation='relu', padding='same'),
+        keras.layers.MaxPool2D(pool_size=(2, 2), strides=(1, 1)),
+        keras.layers.Conv2D(filters=25, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
+                            activation='relu', padding='same'),
+        keras.layers.Conv2D(filters=25, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
+                            activation='relu', padding='same'),
+        keras.layers.MaxPool2D(pool_size=(2, 2), strides=(1, 1)),
+        keras.layers.Flatten(),
+        keras.layers.Dense(128, activation="relu"),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(64, activation="relu"),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(2, activation='softmax')
+    ])
 
-    print(f"\nTotal samples: {y_pred.shape[0]}")
-    print(f"Total positive: {np.sum(y_test)}")
-    print(f"Total negative: {y_test.shape[0] - np.sum(y_test)}")
+    model.compile()
 
-    print(f"f1-score = {f1_score_post_epoch(y_test, y_pred_proba):.2f}")
-    print(f"precision = {precision_post_epoch(y_test, y_pred_proba):.2f}")
-    print(f"recall = {recall_post_epoch(y_test, y_pred_proba):.2f}")
+    experiment_names = [
+        'experiment1_balanced', 'experiment2_balanced_n_10', 'experiment4_balanced', 'experiment5_balanced_5_99',
+        'experiment7_balanced_n_10', 'experiment8_balanced_n_10', 'experiment9_balanced', 'experiment10_balanced_n_10'
+    ]
 
-alpha = 0.05
-
-model = keras.models.Sequential([
-    #keras.layers.InputLayer(shape=[144, 480, 1]),
-    keras.layers.BatchNormalization(),
-    keras.layers.Conv2D(filters=10, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
-                        activation='relu', padding='same'),
-    keras.layers.MaxPool2D(pool_size=(3, 3), strides=(1, 1)),
-    keras.layers.Conv2D(filters=20, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
-                        activation='relu', padding='same'),
-    keras.layers.Conv2D(filters=20, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
-                        activation='relu', padding='same'),
-    keras.layers.MaxPool2D(pool_size=(2, 2), strides=(1, 1)),
-    keras.layers.Conv2D(filters=25, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
-                        activation='relu', padding='same'),
-    keras.layers.Conv2D(filters=25, kernel_size=(3, 3), strides=(1, 1), kernel_regularizer=l2(0.2),
-                        activation='relu', padding='same'),
-    keras.layers.MaxPool2D(pool_size=(2, 2), strides=(1, 1)),
-    keras.layers.Flatten(),
-    keras.layers.Dense(128, activation="relu"),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(64, activation="relu"),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(2, activation='softmax')
-])
-
-model.compile()
-
-# Restore the weights
-model.load_weights('hybridconv1_nonoise_all_gulkana_neg')
-
-X_test, y_test = preprocess_real_data('thesis_real_data_labels.csv', 'real_data_metadata.csv')
-X_test = np.transpose(expand_dim(X_test), axes=(0, 2, 1, 3))
-
-print("Preprocessing complete")
-print(f"type(X) = {type(X_test)}")
-print(f"X.shape = {X_test.shape}")
-print(f"y.shape = {y_test.shape}")
-
-
-## Balance dataset by omitting positive or negative samples as needed
-n_positive = np.sum(y_test)
-n_negative = y_test.shape[0] - n_positive
-
-print(f"Dataset contains {n_positive} positive samples and {n_negative} negative samples.")
-
-# y_test_positive, X_test_positive = np.where(y_test, y_test, X_test)
-# y_test_negative, X_test_negative = np.where(~y_test, y_test, X_test)
-#
-# if n_positive > n_negative:
-#     # More positive than negative samples
-#     positive_indices = np.random.choice(len(n_positive), size=n_negative, replace=False)
-#     y_test = np.concatenate([y_test_negative, y_test_positive[positive_indices]])
-#     X_test = np.concatenate([X_test_negative, X_test_positive[positive_indices, :, :]])
-# else:
-#     # More negative than positive samples
-#     negative_indices = np.random.choice(len(n_negative), size=n_positive, replace=False)
-#     y_test = np.concatenate([y_test_positive, y_test_negative[negative_indices]])
-#     X_test = np.concatenate([X_test_positive, X_test_negative[negative_indices, :, :]])
-
-y_pred_proba = model.predict(X_test)
-y_pred = np.argmax(y_pred_proba, axis=1)
-
-print(y_test)
-print(y_pred)
-
-print(y_pred_proba)
-
-print(f"\nTotal samples: {y_pred.shape[0]}")
-print(f"Total positive: {np.sum(y_test)}")
-print(f"Total negative: {y_test.shape[0] - np.sum(y_test)}")
-
-print(f"f1-score = {f1_score_post_epoch(y_test, y_pred_proba):.2f}")
-print(f"precision = {precision_post_epoch(y_test, y_pred_proba):.2f}")
-print(f"recall = {recall_post_epoch(y_test, y_pred_proba):.2f}")
+    for experiment_name in experiment_names:
+        predict_real(model, experiment_name, X_test, y_test)
